a499a264f70ec00b40910f55decc63aca13e9152
Oscar Gomez <oscargomezgomezluna@gmail.com>
2023-10-02 18:07:46 -0500

    card y inicio

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import useFetch from '../hooks/useFetch'
 import { useNavigate } from 'react-router-dom'
+import './layouts/pokecard.css'
 
 const pokeCard = ({ url}) => {
 
@@ -18,26 +19,26 @@ const pokeCard = ({ url}) => {
   
 
   return (
-    <article onClick={handleNavigate}>
-      <header>
-          <img src={pokemon?.sprites.other['official-artwork'].front_default} alt="" />
+    <article className='pokecard' onClick={handleNavigate}>
+      <header className={`pokecard_header ${pokemon?.types[0].type.name}-gradient`}>
+          <img className='pokecard_img' src={pokemon?.sprites.other['official-artwork'].front_default} alt="" />
       </header>
-      <section>
-        <h3>{pokemon?.name}</h3>
-        <ul>
+      <section className='pokecard_body'>
+        <h3 className='pokecard_name'>{pokemon?.name}</h3>
+        <ul className='pokecard_types'>
           {
             pokemon?.types.map(typeInfo => 
-              <li key={typeInfo.type.url}>{typeInfo.type.name}</li>
+              <li className='pokecard_typename' key={typeInfo.type.url}>{typeInfo.type.name}</li>
               )
           }
         </ul>
-        <hr />
-        <ul>
+        <hr className='pokecard_hr' />
+        <ul className='pokecard_stats'>
           {
             pokemon?.stats.map(statInfo => (
-              <li key={statInfo.stat.url}>
-                <span>{statInfo.stat.name}</span>
-                <span>{statInfo.base_stat}</span>
+              <li className='pokecard_stats' key={statInfo.stat.url}>
+                <span className='pokecard_stat_name'>{statInfo.stat.name}</span>
+                <span className='pokecard_stat_base'>{statInfo.base_stat}</span>
               </li>
             ))
           }
